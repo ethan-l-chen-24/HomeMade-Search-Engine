bb939283e3177c9a5dd03ab276e8c2a6af19189d
Ethan L. Chen <f004h1g@plank.thayer.dartmouth.edu>
2021-10-21 22:03:56 -0400

    modified testing.sh

--- indexer/testing.sh
+++ indexer/testing.sh
@@ -1,6 +1,9 @@
 
 # ETHAN CHEN
-# testing.sh - testing file for index module
+# testing.sh - testing file for indexer and indextest module
+
+########### INDEXER #############
+#################################
 
 # LETTERS TEST
 # -------------
@@ -35,3 +38,46 @@
 
 # WRONG NUMBER OF ARGUMENTS
 ./indexer sdfjkldsj sdfjkldjs 23 vdsjlkj thislabiskillingme
+
+# VALGRIND
+make valgrind
+
+########### INDEXTEST #############
+###################################
+
+# LETTERS TEST
+# -------------
+./indextest letters-index-0 letters-index-0-test
+
+./indextest letters-index-1 letters-index-1-test
+
+./indextest letters-index-2 letters-index-2-test
+
+./indextest letters-index-3 letters-index-3-test
+
+./indextest letters-index-4 letters-index-4-test
+
+./indextest letters-index-5 letters-index-5-test
+
+./indextest letters-index-6 letters-index-6-test
+
+# TOSCRAPE TEST
+# -------------
+./indextest toscrape-index-0 toscrape-index-0-test
+
+./indextest toscrape-index-1 toscrape-index-1-test
+
+# WIKIPEDIA TESTS
+# ---------------
+./indextest wikipedia-index-0 wikipedia-index-0-test
+
+./indextest wikipedia-index-1 wikipedia-index-1-test
+
+# NONEXISTENT file
+./indextest non-existent-file filename
+
+# WRONG NUMBER OF ARGUMENTS
+./indextest sdfjkldsj sdfjkldjs 23 vdsjlkj thislabiskillingme
+
+# VALGRIND
+make valgrind2
