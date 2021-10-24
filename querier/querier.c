ea73d4353c7ccafbb6b3125af88d5b6017b66ec7
Ethan L. Chen <f004h1g@plank.thayer.dartmouth.edu>
2021-10-24 17:33:01 -0400

    created query directory

---
+++ querier/querier.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "index.h"
+#include "word.h"
+#include "pagedir.h"
+
+bool query();
+
+int main(const int argc, int* argv[])
+{
+
+}
+
+bool query()
+{
+    
+}
+
