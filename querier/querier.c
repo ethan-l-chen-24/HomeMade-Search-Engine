b60aeb35c5773d62ee1b60bec371918fb3cbb992
Ethan L. Chen <f004h1g@plank.thayer.dartmouth.edu>
2021-10-25 16:51:46 -0400

    minimal commenting

--- querier/querier.c
+++ querier/querier.c
@@ -1,3 +1,15 @@
+/* 
+ * querier.c - querier module for tiny search engine 
+ *
+ * given a directory previously accessed and filled by the crawler and a file filled 
+ * by the indexer, the query takes "queries" from the user through stdin and returns
+ * a list of URLs that best match that query by a score calculated as the number of
+ * occurrences of the given word conditions in those webpages according to the query.
+ * This list will be ranked, starting with the URL with the highest score
+ *
+ * Ethan Chen, Oct. 2021
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -10,14 +22,30 @@
 #include "counters.h"
 #include "memory.h"
 
+/************* function prototypes ********************/
+
 bool query(char* pageDirectory, char* indexFilename);
 void processQuery(char* search, index_t* index, char* pageDirectory);
 int countWordsInQuery(char* query);
 char** parseQuery(char* query, int numWords);
 void normalizeQuery(char** words, int numWords);
-counters_t* getIDScores(char** words, index_t* index, char* pageDirectory);
+counters_t* getIDScores(char** words, int numWords, index_t* index, char* pageDirectory);
 
-
+/************** main() ******************/
+/* the "testing" function/main function, which takes two arguments 
+ * as inputs (other than the executable call), the directory containing the
+ * crawler directory and the name of the index file 
+ * 
+ * Pseudocode:
+ *      1. Make sure there are exactly 2 arguments
+ *      2. copy the pageDirectory and indexFilename into malloc'd strings
+ *      3. validate the directory and indexFile
+ *      4. call the querier method
+ * 
+ * Assumptions:
+ *      1. the user puts in valid inputs, otherwise throws errors
+ *      2. the directory exists, otherwise throws errors
+*/
 int main(const int argc, char* argv[])
 {
     char* program = argv[0];
@@ -45,6 +73,7 @@ int main(const int argc, char* argv[])
     } 
     strcpy(indexFilename, indexFnameArg);
 
+    // make sure the directory is a valid crawler directory
     if (!pageDirValidate(pageDir)) {
         count_free(indexFilename);
         count_free(pageDir);
@@ -52,12 +81,13 @@ int main(const int argc, char* argv[])
         return false;
     }
 
+    // check to see if the index file can be opened
     char* trueFilename = stringBuilder(NULL, indexFilename);
     if(fopen(trueFilename, "r") == NULL) {
         fprintf(stderr, "Error: provided filename %s is invalid\n", indexFilename);
     }
 
-    // run the indexer
+    // run the querier
     if (query(pageDir, indexFilename)) {
         printf("SUCCESS!\n\n");
         return 0;
@@ -68,6 +98,18 @@ int main(const int argc, char* argv[])
 
 }
 
+/************** query() ******************/
+/* The skeleton code for the query, which loads the index, 
+ * reads from stdin with queries, and processes them
+ * 
+ * Pseudocode:
+ *      1. load the index
+ *      2. keep on taking from stdin while the query is active
+ *      3. process those queries
+ * 
+ * Assumptions:
+ *      1. the user puts in valid inputs, otherwise throws errors
+*/
 bool query(char* pageDirectory, char* indexFilename)
 {
     FILE* fp = stdin;
@@ -88,12 +130,17 @@ bool query(char* pageDirectory, char* indexFilename)
     }
 }
 
+/************** processQuery() ******************/
+/*
+ *
+ *
+*/
 void processQuery(char* query, index_t* index, char* pageDirectory) 
 {
     int numWords = countWordsInQuery(query);
 
 #ifdef TEST
-    printf("there are %d nums\n", numWords);
+        printf("there are %d nums\n", numWords);
 #endif
 
     char** words = parseQuery(query, numWords);
@@ -111,10 +158,13 @@ void processQuery(char* query, index_t* index, char* pageDirectory)
     }
 #endif
 
-  //  counters_t* set = getIDScores(words, index, pageDirectory);
+    counters_t* idScores = getIDScores(words, numWords, index, pageDirectory);
+    if(idScores == NULL) return;
     // begin reading through files and assigning scores
 }
 
+/************** countWordsInQuery() ******************/
+/* count the number of words in a sentence */
 int countWordsInQuery(char* query)
 {
     int count = 0;
@@ -131,6 +181,11 @@ int countWordsInQuery(char* query)
     return count;
 }
 
+/************** parseQuery() ******************/
+/*
+ *
+ *
+*/
 char** parseQuery(char* query, int numWords)
 {
     char** words = calloc(numWords, sizeof(char*));
@@ -158,6 +213,8 @@ char** parseQuery(char* query, int numWords)
     return words;
 }
 
+/************** normalizeQuery() ******************/
+/* make a list of words all lowercase */
 void normalizeQuery(char** words, int numWords) 
 {
     if(words == NULL) return;
@@ -169,10 +226,15 @@ void normalizeQuery(char** words, int numWords)
 
 }
 
-counters_t* getIDScores(char** words, index_t* index, char* pageDirectory) 
+/************** getIDScores() ******************/
+/*
+ *
+ *
+*/
+counters_t* getIDScores(char** words, int numWords, index_t* index, char* pageDirectory) 
 {
+    if(words == NULL || index == NULL || pageDirectory == NULL) return NULL;
     counters_t* idScores = counters_new();
-    for (char** p = words; *p != NULL; p++) {
 
-    }
 }
+
